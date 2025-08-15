0fd498ba5f06b9db72502c9a9e80870dd84b259e
aaguilard28 <mailaaguilard@gmail.com>
2025-08-14 22:26:48 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -811,7 +811,7 @@ const App = () => {
         display: flex;
         height: 100%;
         width: max-content;
-        animation: marquee 60s linear infinite;
+        animation: marquee 54s linear infinite;
         will-change: transform;
       }
       .marquee-container.paused { animation-play-state: paused; }
