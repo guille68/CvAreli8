d113b5716acfa30a614897d8a044e60c6bb5769e
aaguilard28 <mailaaguilard@gmail.com>
2025-08-14 21:51:35 -0600

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // ← necesario para que el toggle de tema funcione
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
