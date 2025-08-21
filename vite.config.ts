4f09425487b163e960c49fed8072a9ac92f2d351
Guillermo Arias <36457953+guille68@users.noreply.github.com>
2025-08-21 15:33:38 -0300

    modif

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/CvAreli8/' : '/', // <-- Cambia 'CvAreli8' si tu repo tiene otro nombre
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
